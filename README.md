3ffdaddb94d743eff429745c9ca88b1076b330ee
Luo <547605677@qq.com>
2021-11-25 14:31:51 +0800

    add chrome import

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# navigation
+# Name - OneNav
+
+## Introduction - 介绍
+本项目为了解决不同浏览器间的书签同步问题，提供书签主页，这样只要记住OneNav的账号密码，不同浏览器间的书签就能共享。
+
+## Requirements - 技术栈
+
+Java 11+, Spring Boot, Sqlite, Thymeleaf
+
+## Installation - 安装
+
+拉取源码后，mvn package， java -jar xxxx.jar
+
+## Usage - 用法
+
+服务成功启动后，访问浏览器 ip:8991 端口
+
+## Development - 开发
+
+本产品遵循MVC, 前后端不分离开发(就是偷懒
+
+## Changelog - 更新日志
+
+
+
+## FAQ - 常见问题
+
+### Contact - 联系
+
+547605677@qq.com
--- src/main/java/com/example/navigation/config/WebConfigure.java
+++ src/main/java/com/example/navigation/config/WebConfigure.java
@@ -5,7 +5,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -31,7 +33,16 @@ public class WebConfigure extends WebMvcConfigurerAdapter implements Application
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加拦截的请求，并排除几个不拦截的请求
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/login", "/", "/register");
+                .excludePathPatterns("/login", "/", "/register","/static/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        //这是配置模板页面的路径  配置文件里面已经配置了  所以这里暂时不需要
+        //这是是配置静态资源文件的路径
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        super.addResourceHandlers(registry);
     }
 
 }
